cf6a9b39296abefefe9ebc8a5eabb06eb5c5992c
Nishchay Naresh <nishchay.naresh@gmail.com>
2026-05-27 17:28:00 +0530

    +u user-api

--- user-api/src/main/java/com/nishchay/controller/HelloController.java
+++ user-api/src/main/java/com/nishchay/controller/HelloController.java
@@ -1,7 +1,25 @@
 package com.nishchay.controller;
 
+import com.nishchay.service.HelloService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping("/hello")
+    public ResponseEntity<?> hello(@RequestParam(defaultValue = "World") String name) {
+        String updatedName =  helloService.sayHello(name);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(updatedName);
+    }
 }
